588dbd3e7762d9c782bf9f138fcedae90c6e77bd
majkl zumberi <majklzumberi00@gmail.com>
2021-03-02 21:30:39 +0100

    fix(module): :sparkles: declare components into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarLinkComponent } from './components/navbar-link/navbar-link.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CoreModule } from './core/core.module';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarLinkComponent
   ],
   imports: [
     BrowserModule,
+    PrimeNgModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
