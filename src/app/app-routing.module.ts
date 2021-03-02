b7ca071accd4fbbbec5ace9fc0120b72f62823ef
majkl zumberi <majklzumberi00@gmail.com>
2021-03-02 21:29:38 +0100

    fix(routing): :sparkles: add lazy loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'courses', loadChildren: () => import('./features/courses/courses.module').then(m => m.CoursesModule) },
+  { path: 'students', loadChildren: () => import('./features/students/students.module').then(m => m.StudentsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
