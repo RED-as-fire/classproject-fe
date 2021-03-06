9fac8dd7d2e0e8843f88756cb0bde06880cbc303
majkl zumberi <majklzumberi00@gmail.com>
2021-03-06 15:29:04 +0100

    fix(dependency): :arrow_up: removed Primeng, added nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbActionsModule, NbCardModule, NbLayoutModule, NbThemeModule } from '@nebular/theme';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarLinkComponent } from './components/navbar-link/navbar-link.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CoreModule } from './core/core.module';
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 
 @NgModule({
@@ -16,9 +18,14 @@ import { PrimeNgModule } from './prime-ng/prime-ng.module';
   ],
   imports: [
     BrowserModule,
-    PrimeNgModule,
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbActionsModule,
+    NbCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
